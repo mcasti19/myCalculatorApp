fc41157aa9540abcccf9a340e2617244b22ff214
mcasti19 <castdevel@gmail.com>
2024-07-27 15:39:48 -0400

    .

---
+++ calculator copy.js
@@ -0,0 +1,130 @@
+
+//! REVISAR AUNQUE ESTA INCOMPLETA: https://www.blackbox.ai/chat/c6456658-55e4-4dc7-95f7-3040b3b344df
+
+
+const inputResult = document.getElementById( 'result' );
+// console.log( inputResult );
+
+const elements = {
+    numbers: Array.from( document.querySelectorAll( '.number' ) ),
+    operators: {
+        suma: document.getElementById( '+' ),
+        resta: document.getElementById( '-' ),
+        multiplicacion: document.getElementById( '*' ),
+        division: document.getElementById( '/' ),
+    },
+    reset: document.getElementById( 'reset' ),
+    igual: document.getElementById( '=' ),
+    punto: document.getElementById( '.' ),
+    del: document.getElementById( 'del' ),
+    preResult: document.getElementById( 'preResult' ),
+};
+
+let num1 = 0;
+let num2 = 0;
+
+inputResult.addEventListener( 'input', ( e ) => {
+    const valor = e.target.value;
+    if ( !/^[0-9+*/.-]*$/.test( valor ) ) {
+        e.target.value = valor.replace( /[^0-9+*/.-]/g, '' );
+    }
+    const ultimoCaracter = valor.slice( -1 );
+    if ( ['+', '-', '*', '/'].includes( ultimoCaracter ) && valor.length > 1 ) {
+        const penultimoCaracter = valor.slice( -2, -1 );
+        if ( ['+', '-', '*', '/'].includes( penultimoCaracter ) ) {
+            e.target.value = valor.slice( 0, -1 );
+        }
+    }
+} );
+
+
+elements.numbers.forEach( element => {
+    element.addEventListener( 'click', ( e ) => {
+        num1 = inputResult.value += e.target.textContent;
+        // console.log( num1 );
+    } )
+} );
+
+
+
+//*CLICK EVENTS
+
+elements.operators.suma.addEventListener( 'click', ( e ) => {
+    preResult.textContent = `${ inputResult.value } + `;
+    inputResult.setAttribute( 'placeholder', `${ inputResult.value }` );
+    num2 = inputResult.value += e.target.textContent
+    inputResult.value = null;
+} );
+
+elements.operators.resta.addEventListener( 'click', ( e ) => {
+    preResult.textContent = `${ inputResult.value } - `;
+    inputResult.setAttribute( 'placeholder', `${ inputResult.value }` );
+    num2 = inputResult.value += e.target.textContent
+    inputResult.value = null;
+} );
+
+elements.operators.multiplicacion.addEventListener( 'click', ( e ) => {
+    preResult.textContent = `${ inputResult.value } * `;
+    inputResult.setAttribute( 'placeholder', `${ inputResult.value }` );
+    num2 = inputResult.value += e.target.textContent
+    inputResult.value = null;
+} );
+
+elements.operators.division.addEventListener( 'click', ( e ) => {
+    preResult.textContent = `${ inputResult.value } / `;
+    inputResult.setAttribute( 'placeholder', `${ inputResult.value }` );
+    num2 = inputResult.value += e.target.textContent
+    inputResult.value = null;
+} );
+
+
+elements.igual.addEventListener( 'click', () => {
+    preResult.textContent += inputResult.value
+
+    let isSuma = preResult.textContent.includes( '+' );
+    let isResta = preResult.textContent.includes( '-' );
+    let isMultiplicacion = preResult.textContent.includes( '*' );
+    let isDivision = preResult.textContent.includes( '/' );
+
+    ( isSuma )
+        ? suma( num1, num2 )
+        : ( isResta )
+            ? resta( num1, num2 )
+            : ( isMultiplicacion )
+                ? multiplication( num1, num2 )
+                : ( isDivision )
+                    ? division( num1, num2 )
+                    : null
+
+} )
+
+
+
+
+elements.reset.addEventListener( 'click', () => {
+    inputResult.value = null;
+    preResult.textContent = null;
+    inputResult.setAttribute( 'placeholder', 0 );
+    inputResult.focus();
+} );
+//*FUNCIONES
+
+function suma ( a, b ) {
+    // console.log( 'Estoy sumando o no?' );
+    return inputResult.value = parseFloat( a ) + parseFloat( b );
+}
+
+function resta ( a, b ) {
+    // console.log( 'Estoy restando o no?' );
+    return inputResult.value = parseFloat( a ) - parseFloat( b );
+}
+
+function multiplication ( a, b ) {
+    // console.log( 'Estoy multiplicando o no?' );
+    return inputResult.value = parseFloat( a ) * parseFloat( b );
+}
+
+function division ( a, b ) {
+    // console.log( 'Estoy dividiando o no?' );
+    return inputResult.value = parseFloat( a ) / parseFloat( b );
+}
---
+++ calculator.js
@@ -0,0 +1,175 @@
+//* DECLARACION DE VARIABLES
+const inputResult = document.getElementById( 'result' );
+const preResult = document.getElementById( 'preResult' );
+
+//* DECLARACION DE BOTONES DE LA CALCULADORA
+const elements = {
+    numbers: Array.from( document.querySelectorAll( '.number' ) ),
+    operators: {
+        suma: document.getElementById( '+' ),
+        resta: document.getElementById( '-' ),
+        multiplicacion: document.getElementById( '*' ),
+        division: document.getElementById( '/' ),
+    },
+    reset: document.getElementById( 'reset' ),
+    igual: document.getElementById( 'equal' ),
+};
+
+let valor1 = '';
+let valor2 = '';
+let operacion = '';
+let resultado = '';
+let done = false;
+
+//* EVENTO PARA CAPTURAR LO QUE SE INTRODUCE POR EL TECLADO PERMITIENDO SOLO NUMEROS Y OPERADORES
+
+inputResult.addEventListener( 'input', ( e ) => {
+    const valor = e.target.value;
+    if ( !/^[0-9+*/.-]*$/.test( valor ) ) {
+        e.target.value = valor.replace( /[^0-9+*/.-]/g, '' );
+    }
+    const ultimoCaracter = valor.slice( -1 );
+    if ( ['+', '-', '*', '/'].includes( ultimoCaracter ) && valor.length > 1 ) {
+        const penultimoCaracter = valor.slice( -2, -1 );
+        if ( ['+', '-', '*', '/'].includes( penultimoCaracter ) ) {
+            e.target.value = valor.slice( 0, -1 );
+        }
+    }
+} );
+
+
+//* EVENTO PARA CAPTURAR EL VALOR DE LOS BOTONES AL HACERLE CLICK
+// elements.numbers.forEach( element => {
+//     element.addEventListener( 'click', ( e ) => {
+//         // console.log( "OPERACION: ", operacion, done );
+//         if ( done ) clearAll();
+//         done = false
+
+//         if ( !operacion && !done ) {
+//             done = false;
+//             console.log( "Pero que carajo", done );
+//             valor1 = inputResult.value += e.target.textContent;
+//         } else {
+//             valor2 = inputResult.value += e.target.textContent
+//         }
+//     } )
+// } );
+
+
+elements.numbers.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const value = e.target.textContent;
+        console.log( value );
+        if ( done ) clearAll();
+        console.log( done );
+        done = false
+      if (valor1) {
+          valor2 += value;
+          inputResult.value += value;
+      } else {
+          valor1 += value;
+          inputResult.value += value
+      }
+    });
+  });
+
+
+//* EVENTOS DE LOS OPERADORES +-*/
+
+elements.operators.suma.addEventListener( 'click', () => {
+    if ( valor1 ) {
+        // console.log( valor1 );
+        operacion = '+';
+        preResult.textContent = `${ valor1 } + `;
+        // console.log( inputResult );
+        inputResult.placeholder = inputResult.value;
+        inputResult.value = '';
+    }
+} );
+
+elements.operators.resta.addEventListener( 'click', () => {
+    if ( valor1 ) {
+        console.log( valor1 );
+        operacion = '-';
+        preResult.textContent = `${ valor1 } - `;
+        console.log( inputResult );
+        inputResult.placeholder = inputResult.value;
+        inputResult.value = '';
+    }
+} );
+
+elements.operators.multiplicacion.addEventListener( 'click', () => {
+    if ( valor1 ) {
+        console.log( valor1 );
+        operacion = '*';
+        preResult.textContent = `${ valor1 } * `;
+        console.log( inputResult );
+        inputResult.placeholder = inputResult.value;
+        inputResult.value = '';
+    }
+} );
+
+elements.operators.division.addEventListener( 'click', () => {
+    if ( valor1 ) {
+        console.log( valor1 );
+        operacion = '/';
+        preResult.textContent = `${ valor1 } / `;
+        console.log( inputResult );
+        inputResult.placeholder = inputResult.value;
+        inputResult.value = '';
+    }
+} );
+
+//* EVENTO DEL BOTON IGUAL
+elements.igual.addEventListener('click', () => {
+    if (valor1 && valor2 && operacion) {
+      resultado = realizarOperacion(valor1, valor2, operacion);
+      preResult.textContent = `${valor1} ${operacion} ${valor2} = ${resultado}`;
+        inputResult.value = resultado;
+        done = true;
+    }
+  });
+
+
+//* EVENTO PARA EL RESET
+elements.reset.addEventListener( 'click', clearAll );
+
+
+//*FUNCION PARA LIMPIAR EL IMPUT DEL RESULTADO Y EL PRE-RESULTADO
+function clearAll () {
+    valor1 = '';
+    valor2 = '';
+    operacion = '';
+    inputResult.value = '';
+    inputResult.placeholder = 0;
+    preResult.textContent = '';
+}
+
+//* FUNCION PARA LAS OPERACIONES DE +-*/
+
+function realizarOperacion ( a, b, operacion ) {
+    switch ( operacion ) {
+        case '+':
+            return parseFloat( a ) + parseFloat( b );
+        case '-':
+            return parseFloat( a ) - parseFloat( b );
+        case '*':
+            return parseFloat( a ) * parseFloat( b );
+        case '/':
+            return parseFloat( a ) / parseFloat( b );
+        default:
+            return '';
+    }
+}
+
+
+// const operations = {
+//     '+': (a, b) => parseFloat(a) + parseFloat(b),
+//     '-': (a, b) => parseFloat(a) - parseFloat(b),
+//     '*': (a, b) => parseFloat(a) * parseFloat(b),
+//     '/': (a, b) => parseFloat(a) / parseFloat(b),
+//   };
+  
+//   function realizarOperacion(a, b, operacion) {
+//     return operations[operacion](a, b);
+//   }
